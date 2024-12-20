14cb229ad3784bc7f10844cde30a33283d782fab
abdallammud <abdallammud@gmail.com>
2024-12-20 23:17:08 +0300

    Payroll

--- app/db.php
+++ app/db.php
@@ -1,13 +1,13 @@
 <?php 
-$servername = "localhost";
-$username   = "root";
-$password   = "";
-$db = "asheeri";
-
 // $servername = "localhost";
-// $username   = "u264673343_payroll";
-// $password   = "Hooyomcn94@";
-// $db = "u264673343_payroll";
+// $username   = "root";
+// $password   = "";
+// $db = "asheeri";
+
+$servername = "localhost";
+$username   = "u138037914_payroll";
+$password   = "Hooyomcn94#";
+$db = "u138037914_payroll";
 
 $GLOBALS['conn'] = $conn = new mysqli($servername, $username, $password, $db);
 
